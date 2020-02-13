8b664210084d667506b447b9cd82b261b6570108
Veysel Erden <12845347+veyselerden@users.noreply.github.com>
2020-02-14 02:34:49 +0300

    Add files via upload

---
+++ enable-dark-theme.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+#ENABLE GTK DARK THEME
+
+cat > ~/.outfile <<DELIM
+[Settings]
+gtk-application-prefer-dark-theme=1
+DELIM
+
+#ENABLE PLANK DARK THEME
+
+sudo mv /usr/share/themes/elementary/plank/dock.theme /usr/share/themes/elementary/plank/dock.theme.light.backup
+sudo mv /usr/share/themes/elementary/plank/dock-dark.theme /usr/share/themes/elementary/plank/dock.theme
+
+echo "Log out and log in back to enable the dark theme."
+
+sleep 3 
+
+exit
